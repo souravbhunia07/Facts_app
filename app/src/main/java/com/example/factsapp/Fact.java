46e7c9da2bb8dc0241a18191f634ef70b11ea05b
Sourav Kumar Bhunia <souravbhunia400@gmail.com>
2022-09-19 21:15:51 +0530

    Facts app

---
+++ app/src/main/java/com/example/factsapp/Fact.java
@@ -0,0 +1,44 @@
+package com.example.factsapp;
+
+public class Fact {
+    public static String[] getFacts() {
+        return new String[] {
+                "The Hawaiian alphabet has only 12 letters.",
+                "A ball of glass will bounce higher than a ball of rubber of the same size.",
+                "Termites eat through wood 2 times faster when listening to rock music.",
+                "The bullfrog is the only animal that never sleeps.",
+                "Elephants are capable of swimming 20 miles a day.",
+                "Elephants are the only mammals that can't jump.",
+                "Cats can hear ultrasound.",
+                "Despite its hump, a camel has a straight spine.",
+                "A human brain weighs around 3 pounds.",
+                "The Hawaiian alphabet has only 12 letters.",
+                "A ball of glass will bounce higher than a ball of rubber of the same size.",
+                "Termites eat through wood 2 times faster when listening to rock music.",
+                "The bullfrog is the only animal that never sleeps.",
+                "Elephants are capable of swimming 20 miles a day.",
+                "Elephants are the only mammals that can't jump.",
+                "Cats can hear ultrasound.",
+                "Despite its hump, a camel has a straight spine.",
+                "A human brain weighs around 3 pounds.",
+                "The Hawaiian alphabet has only 12 letters.",
+                "A ball of glass will bounce higher than a ball of rubber of the same size.",
+                "Termites eat through wood 2 times faster when listening to rock music.",
+                "The bullfrog is the only animal that never sleeps.",
+                "Elephants are capable of swimming 20 miles a day.",
+                "Elephants are the only mammals that can't jump.",
+                "Cats can hear ultrasound.",
+                "Despite its hump, a camel has a straight spine.",
+                "A human brain weighs around 3 pounds.",
+                "The Hawaiian alphabet has only 12 letters.",
+                "A ball of glass will bounce higher than a ball of rubber of the same size.",
+                "Termites eat through wood 2 times faster when listening to rock music.",
+                "The bullfrog is the only animal that never sleeps.",
+                "Elephants are capable of swimming 20 miles a day.",
+                "Elephants are the only mammals that can't jump.",
+                "Cats can hear ultrasound.",
+                "Despite its hump, a camel has a straight spine.",
+                "A human brain weighs around 3 pounds."
+        };
+    }
+}
---
+++ app/src/main/java/com/example/factsapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.factsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView factTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        factTextView = findViewById(R.id.txt_fact);
+
+        String[] myFacts = Fact.getFacts();
+        for (String fact : myFacts) {
+            factTextView.append(fact + "\n\n");
+        }
+    }
+}
